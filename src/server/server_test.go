e2c557cb84b2a3fd88d4d9299a5432b829e39e5e
Gradey Cullins <gradeycullins@gmail.com>
2023-07-16 16:20:52 -0600

    Revert broken hosted image test approach
    Add table tests for server pkg

--- src/server/server_test.go
+++ src/server/server_test.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -12,6 +13,7 @@ import (
 	"purity-vision-filter/src/db"
 	"purity-vision-filter/src/images"
 	"testing"
+	"time"
 
 	"github.com/go-pg/pg/v10"
 )
@@ -23,9 +25,15 @@ func (s *TestServe) Init(_conn *pg.DB) {
 	conn = _conn
 }
 
-func TestHealthHandler(t *testing.T) {
-	testHealthNoBody(t)
-	testHealthJunkBody(t)
+type FilterTestExpect struct {
+	Code  int
+	Error error
+	Res   []*images.ImageAnnotation
+}
+
+type FilterTest struct {
+	Given  []string
+	Expect FilterTestExpect
 }
 
 func testHealthNoBody(t *testing.T) {
@@ -75,18 +83,19 @@ func testHealthJunkBody(t *testing.T) {
 	}
 }
 
-const testURIServerAddr = ":8181"
-const testURIServerURL = "http://localhost:8181"
-
-func getTestServer() *http.Server {
-	http.Handle("/", http.FileServer(http.Dir("../../fixtures")))
-
-	return &http.Server{
-		Addr: testURIServerAddr,
-	}
+func TestHealthHandler(t *testing.T) {
+	testHealthNoBody(t)
+	testHealthJunkBody(t)
 }
 
-func TestFilterEmpty(t *testing.T) {
+func beforeTest() {
+	conn, _ := db.Init(config.DefaultDBTestName)
+	defer conn.Close()
+	conn.Model(&images.ImageAnnotation{}).Where("1=1").Delete()
+}
+
+func TestFilterHandlerTable(t *testing.T) {
+	t.Cleanup(beforeTest)
 	conn, err := db.Init(config.DefaultDBTestName)
 	if err != nil {
 		log.Fatal(err)
@@ -96,112 +105,155 @@ func TestFilterEmpty(t *testing.T) {
 	s := TestServe{}
 	s.Init(conn)
 
-	req := &AnnotateReq{
-		ImgURIList: []string{},
-	}
-
-	var errRes ErrorRes
-	res, err := testBatchImgFilterHandler(req)
-	if err != nil {
-		t.Error("Shouldn't have thrown an error")
-	}
-
-	decoder := json.NewDecoder(res.Body)
-	if err := decoder.Decode(&errRes); err != nil {
-		t.Error("JSON body missing or malformed")
-	}
-
-	if res.Code != 400 || errRes.Message != "ImgUriList cannot be empty" {
-		t.Error("Web server should have returned a 400 because the ImgURIList was empty")
-	}
-}
-
-func TestFilter(t *testing.T) {
-	// init local fileserver
-	fs := getTestServer()
-
-	defer fs.Close()
-
-	conn, err := db.Init(config.DefaultDBTestName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	s := TestServe{}
-	s.Init(conn)
-
-	req := &AnnotateReq{
-		ImgURIList: []string{
-			"http://localhost:8181/image-1.jpg",
-			"http://localhost:8181/image-2.jpg",
-			"http://localhost:8181/image-3.jpg",
+	tests := []FilterTest{
+		{
+			Given: []string{},
+			Expect: FilterTestExpect{
+				Code:  400,
+				Error: errors.New("ImgUriList cannot be empty"),
+				Res:   []*images.ImageAnnotation{},
+			},
+		},
+		{
+			Given: []string{
+				"https://i.imgur.com/FEpwOY8.jpg",
+				"https://i.imgur.com/FEpwOY8.jpg",
+				"https://i.imgur.com/FEpwOY8.jpg",
+				"https://i.imgur.com/FEpwOY8.jpg",
+				"https://i.imgur.com/FEpwOY8.jpg",
+			},
+			Expect: FilterTestExpect{
+				Code:  200,
+				Error: nil,
+				Res: []*images.ImageAnnotation{
+					{
+						Hash:      "87408bebb6a1d42cd7cc1bbffb6d7dcc6aff14af4aea5c9af9fc5b624cf7c93a",
+						URI:       "https://i.imgur.com/FEpwOY8.jpg",
+						Error:     sql.NullString{},
+						DateAdded: time.Now(),
+						Adult:     2,
+						Spoof:     1,
+						Medical:   2,
+						Violence:  3,
+						Racy:      5,
+					},
+				},
+			},
+		},
+		{
+			Given: []string{
+				"https://i.imgur.com/FEpwOY8.jpg",
+				"https://i.imgur.com/6ZOubbU.png",
+				"https://i.imgur.com/qtTfzH6.jpg",
+				"https://i.imgur.com/RwHI4jk.jpg",
+			},
+			Expect: FilterTestExpect{
+				Code:  200,
+				Error: nil,
+				Res: []*images.ImageAnnotation{
+					{
+						Hash:      "87408bebb6a1d42cd7cc1bbffb6d7dcc6aff14af4aea5c9af9fc5b624cf7c93a",
+						URI:       "https://i.imgur.com/FEpwOY8.jpg",
+						Error:     sql.NullString{},
+						DateAdded: time.Now(),
+						Adult:     2,
+						Spoof:     1,
+						Medical:   2,
+						Violence:  3,
+						Racy:      5,
+					},
+					{
+						Hash:      "65d2ad788998a350e7476c4a110ece346d4d56ab76670d48ddd896444a0029b1",
+						URI:       "https://i.imgur.com/6ZOubbU.png",
+						Error:     sql.NullString{},
+						DateAdded: time.Now(),
+						Adult:     1,
+						Spoof:     1,
+						Medical:   2,
+						Violence:  1,
+						Racy:      5,
+					},
+					{
+						Hash:      "2a5cdbc5148669ec4efc788d03f535cf99f13756ccd200ae48faf59fac30b811",
+						URI:       "https://i.imgur.com/qtTfzH6.jpg",
+						Error:     sql.NullString{},
+						DateAdded: time.Now(),
+						Adult:     2,
+						Spoof:     3,
+						Medical:   2,
+						Violence:  4,
+						Racy:      2,
+					},
+					{
+						Hash:      "b2047dfb0412f815859b269288a948528587b77d9b3e0395cd57faf2ba4c37f5",
+						URI:       "https://i.imgur.com/RwHI4jk.jpg",
+						Error:     sql.NullString{},
+						DateAdded: time.Now(),
+						Adult:     5,
+						Spoof:     1,
+						Medical:   3,
+						Violence:  3,
+						Racy:      5,
+					},
+				},
+			},
 		},
 	}
 
-	res, err := testBatchImgFilterHandler(req)
-	if err != nil {
-		t.Error(err)
-	}
-	if res.Code != 200 {
-		t.Error("Web server should have returned a 200")
-	}
-	var annotation []*images.ImageAnnotation
-	json.Unmarshal(res.Body.Bytes(), &annotation)
-	if len(annotation) != len(req.ImgURIList) {
-		t.Error("Handler didn't return the right results")
-	}
+	for _, test := range tests {
+		req := &AnnotateReq{ImgURIList: test.Given}
+		rec, err := testFilterHandler(req)
 
-	// Cleanup DB
-	for _, uri := range req.ImgURIList {
-		if err = images.DeleteByURI(conn, uri); err != nil {
-			t.Log(err)
+		if test.Expect.Error != nil {
+			decoder := json.NewDecoder(rec.Body)
+			var errRes ErrorRes
+			if err := decoder.Decode(&errRes); err != nil {
+				t.Error("JSON body missing or malformed")
+			}
+			if errRes.Message != test.Expect.Error.Error() {
+				t.Error("expected error but didn't get one")
+			}
+		}
+
+		if test.Expect.Error == nil && err != nil {
+			t.Error("didn't expect error but got: ", err.Error())
+		}
+		if rec.Code != test.Expect.Code {
+			t.Errorf("expected status %d but got %d", rec.Code, test.Expect.Code)
+		}
+		var annotations []*images.ImageAnnotation
+		json.Unmarshal(rec.Body.Bytes(), &annotations)
+
+		if len(annotations) != len(test.Expect.Res) {
+			t.Errorf("expected %d annotation results but got %d", len(test.Expect.Res), len(annotations))
+		}
+
+		for i, annotation := range annotations {
+			expected := test.Expect.Res[i]
+			if annotation.Adult != expected.Adult {
+				t.Errorf("expected adult to be %d but got %d", annotation.Adult, expected.Adult)
+			}
+
+			if annotation.Spoof != expected.Spoof {
+				t.Errorf("expected spoof to be %d but got %d", annotation.Spoof, expected.Spoof)
+			}
+
+			if annotation.Medical != expected.Medical {
+				t.Errorf("expected medical to be %d but got %d", annotation.Medical, expected.Medical)
+			}
+
+			if annotation.Violence != expected.Violence {
+				t.Errorf("expected violence to be %d but got %d", annotation.Violence, expected.Violence)
+			}
+
+			if annotation.Racy != expected.Racy {
+				t.Errorf("expected racy to be %d but got %d", annotation.Racy, expected.Racy)
+			}
 		}
 	}
 }
 
-func TestFilterDuplicates(t *testing.T) {
-	conn, err := db.Init(config.DefaultDBTestName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	s := TestServe{}
-	s.Init(conn)
-	uri := "localhost:8181/image-1.jpg"
-
-	req := &AnnotateReq{
-		ImgURIList: []string{
-			"localhost:8181/image-1.jpg",
-			"localhost:8181/image-1.jpg",
-			"localhost:8181/image-1.jpg",
-			"localhost:8181/image-1.jpg",
-		},
-	}
-
-	//var errRes ErrorRes
-	res, err := testBatchImgFilterHandler(req)
-	if err != nil {
-		t.Error("Shouldn't have thrown an error")
-	}
-
-	if res.Code != 200 {
-		t.Error("Web server should have returned a 200")
-	}
-	var annotation []*images.ImageAnnotation
-	json.Unmarshal(res.Body.Bytes(), &annotation)
-	if len(annotation) != 1 {
-		t.Error("Handler didn't return the right results")
-	}
-
-	// Delete the img from the DB.
-	if err = images.DeleteByURI(conn, uri); err != nil {
-		t.Log(err)
-	}
-}
-
-func testBatchImgFilterHandler(fr *AnnotateReq) (*httptest.ResponseRecorder, error) {
+func testFilterHandler(fr *AnnotateReq) (*httptest.ResponseRecorder, error) {
 	b, err := json.Marshal(fr)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to marshal request body struct")
